93f5e7b674be14d672408c71f07b57141343b550
Fede-deb <fusy@fi.uba.ar>
2025-04-23 19:14:24 -0300

    Struc ListaEnlazada Y Struc Nodo

--- lista_enlazada.go
+++ lista_enlazada.go
@@ -1,1 +1,17 @@
 package lista
+
+type nodoLista[T any] struct{
+	dato	 T
+	siguiente *nodoLista[T]
+}
+
+type ListaEnlazada[T any] struct{
+	
+	primero *nodoLista[T]
+	ultimo *nodoLista[T]
+	largo int
+}
+
+func CrearListaEnlazada[T any]() Lista[T] {
+	return &ListaEnlazada[T]{}
+}
