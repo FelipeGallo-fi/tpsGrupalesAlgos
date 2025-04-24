93ee2bd293bb205d6ec90c45abf0a0043181c0de
Fede-deb <fusy@fi.uba.ar>
2025-04-24 13:21:58 -0300

    traigo los cambios de la rama main  la rama Fede

--- lista_enlazada.go
+++ lista_enlazada.go
@@ -11,10 +11,88 @@ type ListaEnlazada[T any] struct {
 	largo   int
 }
 
+func (lista *ListaEnlazada[T]) Iterar(visitar func(T) bool) {
+	for actual := lista.primero; actual != nil; {
+		if !visitar(actual.dato) {
+			break
+		}
+		actual = actual.siguiente
+	}
+}
+
+type IteradorListaImplementacion[T any] struct {
+	lista    *ListaEnlazada[T]
+	actual   *nodoLista[T]
+	anterior *nodoLista[T]
+}
+
+func (lista *ListaEnlazada[T]) Iterador() IteradorLista[T] {
+	return &IteradorListaImplementacion[T]{
+		lista:    lista,
+		actual:   lista.primero,
+		anterior: nil,
+	}
+}
+
+func (iter *IteradorListaImplementacion[T]) VerActual() T {
+	if iter.actual == nil {
+		panic("El iterador termino de iterar")
+	}
+	return iter.actual.dato
+}
+
+func (iter *IteradorListaImplementacion[T]) HaySiguiente() bool {
+	return iter.actual != nil
+}
+
+func (iter *IteradorListaImplementacion[T]) Siguiente() {
+	if iter.actual == nil {
+		panic("El iterador termino de iterar")
+	}
+	iter.anterior = iter.actual
+	iter.actual = iter.actual.siguiente
+}
+
+func (iter *IteradorListaImplementacion[T]) Insertar(dato T) {
+	if iter.anterior == nil {
+		iter.lista.InsertarPrimero(dato)
+		iter.actual = iter.lista.primero
+	} else if iter.actual == nil {
+		iter.lista.InsertarUltimo(dato)
+		iter.actual = iter.lista.ultimo
+	} else {
+		nuevo := &nodoLista[T]{dato, iter.actual}
+		iter.anterior.siguiente = nuevo
+		iter.actual = nuevo
+		iter.lista.largo++
+	}
+}
+
+func (iter *IteradorListaImplementacion[T]) Borrar() T {
+	if !iter.HaySiguiente() {
+		panic("El iterador termino de iterar")
+	}
+
+	var dato T
+
+	if iter.anterior == nil {
+		dato = iter.lista.BorrarPrimero()
+		iter.actual = iter.lista.primero
+	} else {
+		dato = iter.actual.dato
+		iter.anterior.siguiente = iter.actual.siguiente
+		if iter.actual == iter.lista.ultimo {
+			iter.lista.ultimo = iter.anterior
+		}
+		iter.actual = iter.actual.siguiente
+		iter.lista.largo--
+	}
+
+	return dato
+}
 func CrearListaEnlazada[T any]() Lista[T] {
 	return &ListaEnlazada[T]{}
 }
-
 
 func (l *ListaEnlazada[T]) EstaVacia() bool {
 	return l.largo == 0
@@ -30,7 +108,7 @@ func (l *ListaEnlazada[T]) InsertarPrimero(dato T) {
 	l.primero = nuevo
 	if l.ultimo == nil {
 		l.ultimo = nuevo
-	} 
+	}
 	l.largo++
 }
 
---
+++ lista_test.go
@@ -0,0 +1,84 @@
+package lista_test
+
+import (
+	"testing"
+)
+
+const _VOLUMEN = 1000000
+
+func TestListaVacia(t *testing.T){
+	/*que la lista vacia se comporte como una lista vacia
+	ej:
+		panic al verprimero , borrar ,etc
+
+	*/
+}
+
+func TestListaVolumen(t *testing.T){
+	/*para provar de cargar muchos datos
+	ej:
+		probar que al ir insertando datos del 1 al 1000
+		el l.primero.dato se vaya actualizando
+		*/
+}
+
+
+func TestListas(t *testing.T){
+	/*Este es un test general que pudemos usar para hacer una lista 
+	de varios tipos 
+	ej: 
+		int
+		float64
+		string	
+		bool
+	*/
+}
+func TestListaListas(t *testing.T){
+	//Una lista de listas
+}
+
+func TestInsertar(t *testing.T){
+	/*podemos usarlo para insertar muchos datos intercalando en 
+	ej:	
+		insertar primero y ver ultimo muchas veces
+		ver ultimo muchas veces
+		etc
+	*/
+}
+
+func TestCrearIterador(t *testing.T) {
+	/*Al insertar un elemento en la posición en la que se crea el iterador,
+	 efectivamente se inserta al principio.*/
+}
+
+func TestIteradorAlFinal(t *testing.T) {
+	/*Insertar un elemento cuando el iterador está al final
+	 efectivamente es equivalente a insertar al final.*/
+}
+
+func TestInsertarMedio(t *testing.T) {
+	/*Insertar un elemento en el medio se hace en la posición correcta..*/
+}
+func TestRemoverMedio(t *testing.T) {
+	/*Verificar que al remover un elemento del medio, este no está.*/
+}
+
+func TestRemoverElmento(t *testing.T) {
+	/*Al remover el elemento cuando se crea el iterador, cambia el primer elemento de la lista.*/
+}
+
+func TestRemoverUltimoIteradorElmento(t *testing.T) {
+	/*Remover el último elemento con el iterador cambia el último de la lista..*/
+}
+
+
+func TestIterarListaVacia(t *testing.T) {
+	//iterar lista vacia
+}
+
+func TestIteradorInterno(t *testing.T) {
+	//probar que se pueda reccorer la lisa con VerActual() y ver siguiente 
+}
+
+
+
