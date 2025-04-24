9de1ab7ec947c2b17a62abe2b0b181243c1fe19c
Fede-deb <fusy@fi.uba.ar>
2025-04-24 12:22:47 -0300

    formateo de go

--- lista_enlazada.go
+++ lista_enlazada.go
@@ -1,21 +1,19 @@
 package lista
 
-type nodoLista[T any] struct{
-	dato	 T
+type nodoLista[T any] struct {
+	dato      T
 	siguiente *nodoLista[T]
 }
 
-type ListaEnlazada[T any] struct{
-	
+type ListaEnlazada[T any] struct {
 	primero *nodoLista[T]
-	ultimo *nodoLista[T]
-	largo int
+	ultimo  *nodoLista[T]
+	largo   int
 }
 
 func CrearListaEnlazada[T any]() Lista[T] {
 	return &ListaEnlazada[T]{}
 }
-
 
 
 func (l *ListaEnlazada[T]) EstaVacia() bool {
@@ -26,17 +24,17 @@ func nuevoNodo[T any](dato T) *nodoLista[T] {
 	return &nodoLista[T]{dato: dato}
 }
 
-func (l *ListaEnlazada[T]) InsertarPrimero(dato T){
+func (l *ListaEnlazada[T]) InsertarPrimero(dato T) {
 	nuevo := nuevoNodo(dato)
 	nuevo.siguiente = l.primero
-	l.primero =nuevo
-	if l.ultimo == nil{
+	l.primero = nuevo
+	if l.ultimo == nil {
 		l.ultimo = nuevo
 	} 
 	l.largo++
 }
 
-func (l *ListaEnlazada[T]) InsertarUltimo(dato T){
+func (l *ListaEnlazada[T]) InsertarUltimo(dato T) {
 	nuevo := nuevoNodo(dato)
 	if l.ultimo != nil {
 		l.ultimo.siguiente = nuevo
@@ -48,34 +46,33 @@ func (l *ListaEnlazada[T]) InsertarUltimo(dato T){
 
 }
 
-func (l *ListaEnlazada[T])panicVacia() {
-	if l.EstaVacia(){
+func (l *ListaEnlazada[T]) panicVacia() {
+	if l.EstaVacia() {
 		panic("La lista esta vacia =(")
 	}
 }
-func (l *ListaEnlazada[T])BorrarPrimero() T {
+func (l *ListaEnlazada[T]) BorrarPrimero() T {
 	l.panicVacia()
 	dato := l.primero.dato
-	if l.largo >1 {
+	if l.largo > 1 {
 		l.primero = l.primero.siguiente
 	} else {
 		l.primero = nil
 		l.ultimo = nil
 	}
-	l.largo --
+	l.largo--
 	return dato
 }
 
-func (l *ListaEnlazada[T])VerPrimero() T{
+func (l *ListaEnlazada[T]) VerPrimero() T {
 	l.panicVacia()
 	return l.primero.dato
 }
-func (l *ListaEnlazada[T])VerUltimo() T{
+func (l *ListaEnlazada[T]) VerUltimo() T {
 	l.panicVacia()
 	return l.ultimo.dato
 }
 
-
-func (l *ListaEnlazada[T]) Largo() (dato int){
+func (l *ListaEnlazada[T]) Largo() (dato int) {
 	return l.largo
 }
