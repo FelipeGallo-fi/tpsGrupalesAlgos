cd3ef15507afcdb6f1a85355c977a0aee732456e
Fede-deb <fusy@fi.uba.ar>
2025-05-12 19:34:04 -0300

    ABB

---
+++ diccionario/diccionario_ordenado_test.go
@@ -0,0 +1,62 @@
+package diccionario
+
+import (
+	"testing"
+)
+
+
+func TestABBDiccionarioVacio(t *testing.T) {
+	//misma prueba que en diccionario ver ahi
+}
+func TestABBUnElemento(t *testing.T){
+	//misma prueba que en diccionario ver ahi
+}
+func TestABBGuardar(t *testing.T){
+	//misma prueba que en diccionario ver ahi
+}
+func TestABBReemplazoDato(t *testing.T){
+		//misma prueba que en diccionario ver ahi
+}
+func TestABBBorrar(t *testing.T){
+	//misma prueba que en diccionario ver ahi
+}
+func TestABBReutilizacionDeBorrados(t *testing.T){
+		//misma prueba que en diccionario ver ahi
+}
+func TestABBConClavesNumericas(t *testing.T){
+	//misma prueba que en diccionario ver ahi
+}
+func TestABBConClavesStructs(t *testing.T){
+	//misma prueba que en diccionario ver ahi
+}
+func TestABBClaveVacia(t *testing.T){
+	//misma prueba que en diccionario ver ahi
+}
+func TestABBValorNulo(t *testing.T){
+	//misma prueba que en diccionario ver ahi
+}
+func TestABBVolumen(t *testing.T){
+	//misma prueba que en diccionario ver ahi
+}
+
+
+//POSIBLES PRUEBAS CONCRETAS DE ABB
+
+
+func TestABBIteradorVacio(t *testing.T){
+	
+}
+func TestABBMinMax(t *testing.T)
+func TestABBOInOrder(t *testing.T)
+func TestABBOInOrderConCorte(t *testing.T)
+func TestABBIteradorExterno(t *testing.T){
+	//hacer un reccorido completo usando el iterador externo 
+}
+func TestABBIteradorExternoCorte(t *testing.T){
+	//hacer un reccorido com corte usando el iterador externo 
+}
+
+func TestABBIterador(t *testing.T){
+	//probar que el iterador funciona correctamente
+}
+
